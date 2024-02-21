409a4fdb9525196cfd8daf585f968cc2d8715d3c
kazirumi <tangorumi@gmail.com>
2024-02-22 01:08:10 +0600

    subquery first attempt

---
+++ SQLQuery Subquery variations.sql
@@ -0,0 +1,32 @@
+
+ --multi column subquery data fetching
+--select * 
+--from tblProduct as m
+--where EXISTS (select p.ProductName,p.ProductCost
+--					from tblProductCustomer as pc
+--					inner join tblProduct as p
+--					on pc.Productid_fk= p.ProductId
+--					inner join tblCustomer as c
+--					on c.CustomerId=pc.CustomerId_fk
+--					where p.ProductName=m.ProductName and p.ProductCost=m.ProductCost
+--					group by p.ProductName,p.ProductCost)
+
+ --another multi column subquery data fetching from another level deeper
+--select m.CategoryID,m.Price
+--from Products as m
+--where exists (select CategoryID,Price from
+--                (SELECT p.CategoryID,max(p.Price) as Price
+--                 FROM Products as p
+--                 group by p.CategoryID) as temp
+--              where temp.CategoryID=m.CategoryID and  temp.Price=m.Price
+--             )
+--             order by m.CategoryID
+
+--Highet cost using corelated query
+--select * from tblProduct as p1
+--where 0 = (select count(distinct(p2.ProductCost)) from tblProduct as p2 
+--where p2.ProductCost>p1.ProductCost)
+
+
+--dump data in new table
+--select * into tblProduct2 from tblProduct
